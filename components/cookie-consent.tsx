bf8f66b1e450fb7248f1b90b665cce2300f76418
lehuuhien0701 <55124459+lehuuhien0701@users.noreply.github.com>
2025-12-03 09:15:15 +0700

    fix cookie

--- components/cookie-consent.tsx
+++ components/cookie-consent.tsx
@@ -37,7 +37,7 @@ export const CookieConsent = ({ translations }: CookieConsentProps) => {
     <div className="fixed bottom-10 w-[370px] left-1/2 md:left-10 transform -translate-x-1/2 md:transform-none bg-secondary rounded-[10px] z-50 p-[30px] md:p-6 shadow-lg">
       <div className="flex flex-col items-center justify-between gap-10">
         <div className="flex-1">
-          <h3 className="text-[34px] font-bold text-primary mb-2">
+          <h3 className="text-[34px] font-bold text-white mb-2">
             {translations.title}
           </h3>
           <p className="text-sm leading-5 text-white">
